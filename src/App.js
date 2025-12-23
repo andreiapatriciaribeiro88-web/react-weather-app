4151e3ffc77ceed67a97d69984d6ab18ef0641d4
andreiapatriciaribeiro88-web <andreiapatriciaribeiro88@gmail.com>
2025-12-23 21:14:40 +0000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Andreia Ribeiro ·{" "}
+        <a
+          href="https://github.com/andreiapatriciaribeiro88-web/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Open source
+        </a>{" "}
+        ·
+      </footer>
+    </div>
+  );
 }
