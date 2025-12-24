3e6294fb30fe27dafc88add36ddbcceeab03b54b
andreiapatriciaribeiro88-web <andreiapatriciaribeiro88@gmail.com>
2025-12-24 10:50:25 +0000

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,13 +11,14 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             ></input>
           </div>
           <div className="col-3">
             <input
               type="submit"
               value="Search"
-              className="btn btn-primary"
+              className="btn btn-primary w-100"
             ></input>
           </div>
         </div>
@@ -27,13 +28,18 @@ export default function Weather() {
         <li>Wednesday 09:57</li>
         <li>Partly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
-            alt="Partly Cloudy"
-          />
-          8ºC
+          <div className="d-flex align-items-center">
+            <img
+              src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
+              alt="Partly Cloudy"
+            />
+            <div className="ms-2">
+              <span className="temperature">8</span>
+              <span className="unit">ºC</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
