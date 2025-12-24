c4502fc0c1c337dbee6b0b40457be3c84be942e0
andreiapatriciaribeiro88-web <andreiapatriciaribeiro88@gmail.com>
2025-12-24 09:34:24 +0000

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Andreia Ribeiro ·{" "}
-        <a
-          href="https://github.com/andreiapatriciaribeiro88-web/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Open source
-        </a>{" "}
-        ·
-      </footer>
+      <div className="containernp">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by Andreia Ribeiro ·{" "}
+          <a
+            href="https://github.com/andreiapatriciaribeiro88-web/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Open source
+          </a>{" "}
+          ·
+        </footer>
+      </div>
     </div>
   );
 }
